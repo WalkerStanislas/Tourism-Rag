d7c9951038342f863845c0e6f0843bc5a1201c13
WalkerStanislas <walkercompaore972@gmail.com>
2025-11-05 21:35:44 +0000

    gestion de la quantisation

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -51,7 +51,7 @@ def load_models():
         model_name, 
         device_map="auto",
         #torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
-        low_cpu_mem_usage=True
+        low_cpu_mem_usage=True,
         oad_in_8bit=True,  # quantisation 8-bit pour CPU
     )
     
